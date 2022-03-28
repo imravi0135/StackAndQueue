8d749f70e080168514435c2652dc77ad6de64aa8
ravikishorereddy <ravikishorereddy309@gmail.com>
2022-03-28 07:45:04 +0530

    Welcome to the Stack And Queue

---
+++ StackAndQueue/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Stacks and Queues");
+            Stack stack = new Stack();
+            stack.Push(56);
+            stack.Push(30);
+            stack.Push(70);
+            stack.DisplayStack();
+            Console.ReadKey();
+        }
+    }
+}
